6ffa6100d1066af63d2fd0e1eabffa87f4fc2f6b
Alex Golovin <golovin.alex@gmail.com>
2026-04-07 23:54:10 +0200

    auto: 2026-04-07T23:54

--- src/plx2dist/__init__.py
+++ src/plx2dist/__init__.py
@@ -1,4 +1,13 @@
+import os
+
+os.environ.setdefault("OMP_NUM_THREADS", "1")
+os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
+os.environ.setdefault("MKL_NUM_THREADS", "1")
+os.environ.setdefault("NUMEXPR_NUM_THREADS", "1")
+
 from .conversion import parallax_to_distance, summarize_distance_posterior
+from .pipeline import derive_distances, load_table_to_dataframe, save_dataframe_to_format
+from .cli import build_parser, main
 
 __version__ = "0.1.0"
 
@@ -6,4 +15,9 @@ __all__ = [
     "__version__",
     "parallax_to_distance",
     "summarize_distance_posterior",
+    "derive_distances",
+    "load_table_to_dataframe",
+    "save_dataframe_to_format",
+    "build_parser",
+    "main",
 ]
